cf09874fb9387359ecc120c918f0abc272bb50b5
Fabio-Furtado <f.miguel_furtado@outlook.com>
2018-11-18 03:28:46 +0000

    Adicionados os métodos de permutação de faixas (editado)

--- Testes.java
+++ Testes.java
@@ -3,40 +3,81 @@ public class Testes {
 
             int [][] quadricula = Sudoku2.gq();
             Sudoku2.imprimirQD(quadricula);
-            permutarColunas(quadricula, 1,2);
+            permutarFaixasVerticais(quadricula, 1, 3);
             System.out.println(" ");
             Sudoku2.imprimirQD(quadricula);
             
     }
+
     /**
-     * Faz a permutação dos valores de duas linhas escolhidas numa matriz
-     * Obs: Ter em conta que o índice das linhas começa em 0
+     * Faz a permutação dos valores de duas faixa horizontais escolhidas numa matriz de 9x9.
      * @param matriz matriz a ser modificada
-     * @param linha1 primeira linha a permutar
-     * @param linha2 segunda linha a permutar
+     * @param faixa1 primeira faixa a ser permutar
+     * @param faixa2 segunda faixa a ser permutar
+     * @requires {@code (faixa1 >= 1 && faixa1 <= 3) && (faixa2 >= 1 && faixa2 <=)}
      */
-    public static void permutarLinhas(int[][] matriz, int linha1, int linha2) {
-        int auxiliar; // recebe o valor de matriz[coluna1][i] antes que este seja substituído
-        for (int i = 0; i < matriz.length; i++) {
-            auxiliar = matriz[linha1][i];
-            matriz[linha1][i] = matriz[linha2][i];
-            matriz[linha2][i] = auxiliar;
+    public static void permutarFaixasHorizontais(int[][] matriz, int faixa1, int faixa2) {
+        int auxiliarm, linha1 = 0, coluna2 = 0;
+        switch (faixa1) {
+            case 2:
+                linha1 = 3;
+                break;
+            case 3:
+                linha1 = 6;
+                break;
+        }
+        switch (faixa2) {
+            case 2:
+                linha2 = 3;
+                break;
+            case 3:
+                linha2 = 6;
+                break;
+        }
+        for (int i = 0; i < matriz.length/3; i++) {
+            for (int o = 0; o < matriz.length; o++) {
+                auxiliar = matriz[linha1][o];
+                matriz[linha1][o] = matriz[linha2][o]; 
+                matriz[linha2][o] = auxiliar;
+            }
+            linha1++;
+            linha2++;
         }
     }
-    
+
     /**
-     * Faz a permutação dos valores de duas colunas escolhidas numa matriz
-     * Obs: Ter em conta que o índice das colunas começa em 0
+     * Faz a permutação dos valores de duas faixa verticais escolhidas numa matriz de 9x9
      * @param matriz matriz a ser modificada
-     * @param linha1 primeira coluna a permutar
-     * @param linha2 segunda coluna a permutar
+     * @param faixa1 primeira faixa a permutar
+     * @param faixa2 segunda faixa a permutar
+     * @requires {@code (faixa1 >= 1 && faixa1 <= 3) && (faixa2 >= 1 && faixa2 <=)}
      */
-    public static void permutarColunas(int[][] matriz, int coluna1, int coluna2) {
-        int auxiliar; // recebe o valor de matriz[coluna1][i] antes que este seja substituído
-        for (int i = 0; i < matriz.length; i++) {
-            auxiliar = matriz[i][coluna1];
-            matriz[i][coluna1] = matriz[i][coluna2];
-            matriz[i][coluna2] = auxiliar;
+    public static void permutarFaixasVerticais(int[][] matriz, int faixa1, int faixa2) {
+        int auxiliar, coluna1 = 0, coluna2 = 0;
+        switch (faixa1) {
+            case 2:
+                coluna1 = 3;
+                break;
+            case 3:
+                coluna1 = 6;
+                break;
+        }
+        switch (faixa2) {
+            case 2:
+                coluna2 = 3;
+                break;
+            case 3:
+                coluna2 = 6;
+                break;
+        }
+        for (int i = 0; i < matriz.length/3; i++) {
+            for (int o = 0; o < matriz.length; o++) {
+                auxiliar = matriz[o][coluna1];
+                matriz[o][coluna1] = matriz[o][coluna2]; 
+                matriz[o][coluna2] = auxiliar;
+            }
+            coluna1++;
+            coluna2++;
         }
     }
 }
