1ed52d842f2e78846c1b835621b244f54b9a9385
Fabio-Furtado <f.miguel_furtado@outlook.com>
2018-11-19 10:11:35 +0000

    validade do vetor e da quadrícula (editado)

--- Sudoku2.java
+++ Sudoku2.java
@@ -5,7 +5,10 @@ public class Sudoku2 {
     public static void main(String[] args) {
         int[][] quadricula = gq();
         imprimirQD(quadricula);
+        System.out.println(" ");
+        System.out.println(validadeDaQuadricula(quadricula));
     }
+
     /**
      * imprime uma matriz no terminal
      * @param matriz matriz a ser impriida
@@ -21,117 +24,49 @@ public class Sudoku2 {
     /**
      * Gera uma quadrícula de 9x9 preenchidada com números de 1 a 9
      * @return quadrícula inserida numa matriz
-     */public static int[][] gq () {
+     */
+    public static int[][] gq () {
     int [][] quadricula = new int[9][9]; 
     for(int i = 0; i < quadricula.length; i++) {
         for(int o = 0; o < quadricula.length; o++)
             quadricula[i][o] = (i / 3 + 3 * (i % 3) + o) % 9 + 1;
-        System.out.println();
     }
     return quadricula;
-}
-
-    /**
-     * Faz a permutação dos valores de duas linhas escolhidas numa matriz
-     * Obs: Ter em conta que o índice das linhas começa em 0
-     * @param matriz matriz a ser modificada
-     * @param linha1 primeira linha a permutar
-     * @param linha2 segunda linha a permutar
-     */
-    public static void permutarLinhas(int[][] matriz, int linha1, int linha2) {
-        int auxiliar; // recebe o valor de matriz[coluna1][i] antes que este seja substituído
-        for (int i = 0; i < matriz.length; i++) {
-            auxiliar = matriz[linha1][i];
-            matriz[linha1][i] = matriz[linha2][i];
-            matriz[linha2][i] = auxiliar;
-        }
-    }
-    
-    /**
-     * Faz a permutação dos valores de duas colunas escolhidas numa matriz
-     * Obs: Ter em conta que o índice das colunas começa em 0
-     * @param matriz matriz a ser modificada
-     * @param linha1 primeira coluna a permutar
-     * @param linha2 segunda coluna a permutar
-     */
-    public static void permutarColunas(int[][] matriz, int coluna1, int coluna2) {
-        int auxiliar; // recebe o valor de matriz[coluna1][i] antes que este seja substituído
-        for (int i = 0; i < matriz.length; i++) {
-            auxiliar = matriz[i][coluna1];
-            matriz[i][coluna1] = matriz[i][coluna2];
-            matriz[i][coluna2] = auxiliar;
-        }
-    }
-    
-    /**
-     * Faz a permutação dos valores de duas faixa horizontais escolhidas numa matriz de 9x9.
-     * @param matriz matriz a ser modificada
-     * @param faixa1 primeira faixa a ser permutar
-     * @param faixa2 segunda faixa a ser permutar
-     * @requires {@code (faixa1 >= 1 && faixa1 <= 3) && (faixa2 >= 1 && faixa2 <=)}
-     */
-    public static void permutarFaixasHorizontais(int[][] matriz, int faixa1, int faixa2) {
-        int auxiliarm, linha1 = 0, coluna2 = 0;
-        switch (faixa1) {
-            case 2:
-                linha1 = 3;
-                break;
-            case 3:
-                linha1 = 6;
-                break;
-        }
-        switch (faixa2) {
-            case 2:
-                linha2 = 3;
-                break;
-            case 3:
-                linha2 = 6;
-                break;
-        }
-        for (int i = 0; i < matriz.length/3; i++) {
-            for (int o = 0; o < matriz.length; o++) {
-                auxiliar = matriz[linha1][o];
-                matriz[linha1][o] = matriz[linha2][o]; 
-                matriz[linha2][o] = auxiliar;
-            }
-            linha1++;
-            linha2++;
-        }
     }
 
     /**
-     * Faz a permutação dos valores de duas faixa verticais escolhidas numa matriz de 9x9
-     * @param matriz matriz a ser modificada
-     * @param faixa1 primeira faixa a permutar
-     * @param faixa2 segunda faixa a permutar
-     * @requires {@code (faixa1 >= 1 && faixa1 <= 3) && (faixa2 >= 1 && faixa2 <=)}
+     * Verifica se um vetor tem 9 elementos e se contem os valores de um a 9
+     * @param vetor vetor a ser analizado
+     * @return true se o vetor for válido, false se não o for
      */
-    public static void permutarFaixasVerticais(int[][] matriz, int faixa1, int faixa2) {
-        int auxiliar, coluna1 = 0, coluna2 = 0;
-        switch (faixa1) {
-            case 2:
-                coluna1 = 3;
-                break;
-            case 3:
-                coluna1 = 6;
-                break;
+    public static boolean validadeDoVetor(int[] vetor) {
+        int produto = 1, tamanho = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            produto *= vetor[i];
+            tamanho++;
         }
-        switch (faixa2) {
-            case 2:
-                coluna2 = 3;
+        return ((produto == 1*2*3*4*5*6*7*8*9) && (tamanho == 9))? true : false;
+    }
+
+    public static boolean validadeDaQuadricula(int[][] matriz) {
+        boolean validade = true;
+        int[] vetor = new int[matriz.length]; // recebe a linha/coluna/bloco a ser verfificado
+        for (int i = 0; i < matriz.length; i++) {
+            // linhas
+            for (int j = 0; j < matriz.length; j++)
+                vetor[j] = matriz[i][j]; // o vetor recebe uma linha da matriz por vez para verificação
+            if (!validadeDoVetor(vetor)) {// verificação da validade do vetor
+                validade = false;
                 break;
-            case 3:
-                coluna2 = 6;
-                break;
-        }
-        for (int i = 0; i < matriz.length/3; i++) {
-            for (int o = 0; o < matriz.length; o++) {
-                auxiliar = matriz[o][coluna1];
-                matriz[o][coluna1] = matriz[o][coluna2]; 
-                matriz[o][coluna2] = auxiliar;
             }
-            coluna1++;
-            coluna2++;
+            // colunas
+            for (int j = 0; j < matriz.length; j++)
+                vetor[j] = matriz[j][i]; // o vetor recebe uma coluna da matriz por vez para verificação
+            if (!validadeDoVetor(vetor)) { // verificação da validade do vetor
+                validade = false;
+                break;
+            }
         }
+        return validade;
     }
 }
