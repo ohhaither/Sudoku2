05e2eb3ca4332f412cb50bf5277783a199524a00
Fabio-Furtado <f.miguel_furtado@outlook.com>
2018-11-20 22:47:49 +0000

    adição de métodos

--- Sudoku2.java
+++ Sudoku2.java
@@ -1,6 +1,32 @@
 import java.util.Scanner;
 
 public class Sudoku2 {
+    
+    /**
+     * Gera uma quadrícula de 9x9 preenchidada com números de 1 a 9
+     * @return quadrícula inserida numa matriz
+     */
+    public static int[][] gq () {
+        int [][] quadricula = new int[9][9]; 
+        for(int i = 0; i < quadricula.length; i++) {
+            for(int o = 0; o < quadricula.length; o++)
+                quadricula[i][o] = (i / 3 + 3 * (i % 3) + o) % 9 + 1;
+        }
+        return quadricula;
+    }
+
+    /**
+     * imprime uma matriz no terminal
+     * @param matriz matriz a ser impriida
+    */
+    public static void imprimirQD(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) { // linha
+            for (int j = 0; j < matriz.length; j++) // coluna
+                System.out.print(matriz[i][j] + " ");
+            System.out.println();
+        }
+    }
+        
 
     /**
      * Faz a leitura de um número inteiro no terminal  e certifica que este encontre-se no intervalo 
@@ -20,21 +46,7 @@ public class Sudoku2 {
         } while (num < inicio || num > fim);
         return num;
     }
-
-    // Se inserir espaçoes entre os dígitos haverá um erro
-    /**
-     * Gera uma quadrícula de 9x9 preenchidada com números de 1 a 9
-     * @return quadrícula inserida numa matriz
-     */
-    public static int[][] gq () {
-        int [][] quadricula = new int[9][9]; 
-        for(int i = 0; i < quadricula.length; i++) {
-            for(int o = 0; o < quadricula.length; o++)
-                quadricula[i][o] = (i / 3 + 3 * (i % 3) + o) % 9 + 1;
-        }
-        return quadricula;
-    }
-    
+    // Se adicionar espaços entre os intervalos haverá erro
     /**
      * Lê uma quadrícula inserida linha por linha no terminal
      * @param scan instancia da classe Scanner usada para ler no teminal
@@ -107,5 +119,70 @@ public class Sudoku2 {
             }
         }
         return validade;
+    }
+
+     /**
+     * Faz a permutação dos valores de duas linhas escolhidas numa matriz
+     * Obs: Ter em conta que o índice das linhas começa em 0
+     * @param matriz matriz a ser modificada
+     * @param linha1 primeira linha a permutar
+     * @param linha2 segunda linha a permutar
+     */
+    public static void permutarLinhas(int[][] matriz, int linha1, int linha2) {
+        int auxiliar; // recebe o valor de matriz[coluna1][i] antes que este seja substituído
+        for (int i = 0; i < matriz.length; i++) {
+            auxiliar = matriz[linha1][i];
+            matriz[linha1][i] = matriz[linha2][i];
+            matriz[linha2][i] = auxiliar;
+        }
+    }
+    
+    /**
+     * Faz a permutação dos valores de duas colunas escolhidas numa matriz
+     * Obs: Ter em conta que o índice das colunas começa em 0
+     * @param matriz matriz a ser modificada
+     * @param linha1 primeira coluna a permutar
+     * @param linha2 segunda coluna a permutar
+     */
+    public static void permutarColunas(int[][] matriz, int coluna1, int coluna2) {
+        int auxiliar; // recebe o valor de matriz[coluna1][i] antes que este seja substituído
+        for (int i = 0; i < matriz.length; i++) {
+            auxiliar = matriz[i][coluna1];
+            matriz[i][coluna1] = matriz[i][coluna2];
+            matriz[i][coluna2] = auxiliar;
+        }
+    }
+    
+    /**
+     * Faz a permutação dos valores de duas faixa horizontais escolhidas numa matriz de 9x9.
+     * @param matriz matriz a ser modificada
+     * @param faixa1 primeira faixa a ser permutar
+     * @param faixa2 segunda faixa a ser permutar
+     * @requires {@code (faixa1 >= 0 && faixa1 <= 2) && (faixa2 >= 0 && faixa2 <= 2)}
+     */
+    public static void permutarFaixasHorizontais(int[][] matriz, int faixa1, int faixa2) {
+        int linha1 = faixa1 * 3, linha2 = faixa2 * 3;
+       
+        for (int i = 0; i < matriz.length/3; i++) {
+            permutarLinhas(matriz, linha1, linha2);
+            linha1++;
+            linha2++;
+        }
+    }
+
+    /**
+     * Faz a permutação dos valores de duas faixa verticais escolhidas numa matriz de 9x9
+     * @param matriz matriz a ser modificada
+     * @param faixa1 primeira faixa a permutar
+     * @param faixa2 segunda faixa a permutar
+     * @requires {@code (faixa1 >= 1 && faixa1 <= 3) && (faixa2 >= 1 && faixa2 <=)}
+     */
+    public static void permutarFaixasVerticais(int[][] matriz, int faixa1, int faixa2) {
+        int coluna1 = faixa1 * 3, coluna2 = faixa2 * 3;
+        for (int i = 0; i < matriz.length/3; i++) {
+            permutarColunas(matriz, coluna1, coluna2);
+            coluna1++;
+            coluna2++;
+        }
     }
 }
